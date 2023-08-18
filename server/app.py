a5f4283aea02630351a9e15af23105e2c0bb4a9b
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-08-18 01:56:05 -0600

    lab completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 
 from config import app, db, api
@@ -19,25 +19,47 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
+        user = User(username=json['username'])
+        user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
-        return user.to_dict(), 201
+        session['user_id'] = user.id
+        response = make_response(user.to_dict(), 201)
+        return response
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter(User.id == session['user_id']).first()
+            print(user)
+            response = make_response(user.to_dict(), 200)
+            return response
+        else:
+            return ({}, 204)
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json['username']).first()
+        if user:
+            if user.authenticate(json['password']):
+                session['user_id'] = user.id
+                response = make_response(user.to_dict(), 200)
+                return response
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        response = make_response({}, 202)
+        return response
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
